2cce1b827a2dde4f698d7dcd25927e9458405b28
SirMissir <pkv_96@icloud.com>
2024-01-04 23:53:39 +0300

    feat: dispatch

--- src/reducers/tasksReducer.ts
+++ src/reducers/tasksReducer.ts
@@ -1,38 +1,40 @@
 import {TaskType} from "../TodoList";
 
-export const tasksReducer = (state: TaskType[], action: any) => {
+export const tasksReducer = (state: TaskType[], action: TsarType) => {
     switch (action.type) {
-        case 'XXX': {
+        case 'REMOVE-TASK': {
             return state
         }
         default:
             return state
     }
 }
-type RemoveTaskACType= ReturnType <typeof  removeTaskAC>
+type TsarType = RemoveTaskACType
+
+type RemoveTaskACType = ReturnType <typeof  removeTaskAC>
 
 const removeTaskAC = () =>{
     return {
         type:'REMOVE-TASK'
-    }
+    } as const
 }
 
 
 
 
-export const ButtonReducer = (state: TaskType[], action: any) => {
-    switch (action.type) {
-        case 'XXX': {
-            return state
-        }
-        default:
-            return state
-    }
-}
-type RemoveTaskACType= ReturnType <typeof  removeTaskAC>
-
-const removeTaskAC = () =>{
-    return {
-        type:'REMOVE-TASK'
-    }
-}
+// export const ButtonReducer = (state: TaskType[], action: any) => {
+//     switch (action.type) {
+//         case 'XXX': {
+//             return state
+//         }
+//         default:
+//             return state
+//     }
+// }
+// type RemoveTaskACType= ReturnType <typeof  removeTaskAC>
+//
+// const removeTaskAC = () =>{
+//     return {
+//         type:'REMOVE-TASK'
+//     }
+// }

07ccb8a3cf7008cca010c4d785c892dbe2ed0f7c
SirMissir <pkv_96@icloud.com>
2024-10-08 20:51:04 +0300

    feat: creat component : editableSpan in storybook without changing state

---
+++ src/stories/EditableSpan.stories.tsx
@@ -0,0 +1,37 @@
+import React from 'react';
+import { Meta, StoryFn } from "@storybook/react";
+import { action } from '@storybook/addon-actions';
+import EditableSpan from "../EditableSpan"; // Импортируем action из addon-actions
+
+
+
+const meta: Meta<typeof EditableSpan> = {
+    title: 'Todolist/EditableSpan',
+    component: EditableSpan,
+    tags: ['autodocs'],
+    parameters: {
+        layout: 'fullscreen',
+    },
+    args:{
+        title: 'Title',
+        changeTitle: action('Change Title'),
+    },
+};
+
+export default meta;
+
+// Определяем историю с добавлением элемента
+const Template: StoryFn<{
+    title: string;
+    changeTitle: (newTitle: string) => void;
+}> = (args) => <EditableSpan {...args} />;
+
+// Обработчики для действий
+const changeTaskTitleHandler = action('Change Title');
+
+// Пример обычного состояния
+export const EditableSpanBaseExample = Template.bind({});
+EditableSpanBaseExample.args = {
+    title: 'Title', // Заголовок по умолчанию
+    changeTitle:changeTaskTitleHandler // Передаем обработчик изменения заголовка
+};

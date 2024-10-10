f20d84431e569a90beb0932b34f34a496ad055ce
SirMissir <pkv_96@icloud.com>
2024-10-10 22:20:54 +0300

    refactor:  component decorators

--- src/stories/ReduxStoreProviderDecorator.tsx
+++ src/stories/ReduxStoreProviderDecorator.tsx
@@ -1,14 +1,44 @@
 import React from 'react'
-import AppWithRedux from "../AppWithRedux";
-import {ReduxStoreProviderDecorator} from './ReduxStoreProviderDecorator'
+import {Provider} from "react-redux";
+import {combineReducers, legacy_createStore as createStore} from "redux";
+import {tasksReducer} from "../state/tasks-reducer";
+import {todolistsReducer} from "../state/todolists-reducer";
+import {AppRootStateType} from "../state/store";
+import {v1} from "uuid";
 
 
-export default {
-    title: 'AppWithRedux Component',
-    component: AppWithRedux,
-    decorators:[ReduxStoreProviderDecorator]
-}
 
-export const ReduxStoreProviderDecorator = () => {
-    return <AppWithRedux/>
-}
+
+const rootReducer = combineReducers({
+    tasks: tasksReducer,
+    todolists: todolistsReducer
+})
+
+const initialGlobalState = {
+    todolists: [
+        { id: "todolistId1", title: 'What to learn', filter: 'all' },
+        { id: "todolistId2", title: 'What to buy', filter: 'all' }
+    ],
+    tasks: {
+        ['todolistId1']: [
+            { id: v1(), title: 'CSS', isDone: false },
+            { id: v1(), title: 'JS', isDone: true },
+            { id: v1(), title: 'React', isDone: false }
+        ],
+        ['todolistId2']: [
+            { id: v1(), title: 'bread', isDone: false },
+            { id: v1(), title: 'milk', isDone: true },
+            { id: v1(), title: 'tea', isDone: false }
+        ]
+    }
+};
+
+export const storyBookStore = createStore (rootReducer, initialGlobalState as  AppRootStateType);
+
+export const ReduxStoreProviderDecorator = (storyFn: any) =>(
+
+        <Provider store={storyBookStore}>
+            {storyFn()}
+        </Provider>
+)
+

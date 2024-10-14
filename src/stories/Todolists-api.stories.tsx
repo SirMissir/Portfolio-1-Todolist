d39b5153ddab1f3dd1cd26ab129d5753ef3add07
SirMissir <pkv_96@icloud.com>
2024-10-14 23:54:37 +0300

    feat: creat api request on server with storybook

---
+++ src/stories/Todolists-api.stories.tsx
@@ -0,0 +1,45 @@
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+
+export const GetTodolists = () => {
+    const { state, setState } = useState<any>(null);
+
+    useEffect(() => {
+
+    }, []);
+
+    return <div>{JSON.stringify(state)}</div>;
+};
+
+export const CreateTodolists = () => {
+    const { state, setState } = useState<any>(null);
+
+
+    useEffect(() => {
+
+    }, []);
+
+    return <div>{JSON.stringify(state)}</div>;
+};
+
+export const UpdateTodolists = () => {
+    const { state, setState } = useState<any>(null);
+
+    };
+    useEffect(() => {
+
+    }, []);
+
+    return <div>{JSON.stringify(state)}</div>;
+};
+
+export const DeleteTodolists = () => {
+    const { state, setState } = useState<any>(null);
+
+
+    useEffect(() => {
+
+    }, []);
+
+    return <div>{JSON.stringify(state)}</div>;
+};

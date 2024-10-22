4058f36a382b010fd64e2c628df436c6a2f7c6f9
SirMissir <pkv_96@icloud.com>
2024-10-22 23:52:38 +0300

    feat: creat request

--- src/stories/Todolists-api.stories.tsx
+++ src/stories/Todolists-api.stories.tsx
@@ -2,11 +2,11 @@ import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 
 export default {
-        title: 'API'
+        title: 'API/GetTodolists'
 }
 
 const settings= {
-    withCredetials: true
+    withCredentials: true
 }
 
 
@@ -14,7 +14,7 @@ export const GetTodolists = () => {
     const { state, setState } = useState<any>(null);
 
     useEffect( () => {
-        let promise =axios.get('http://social-network.samuraijs.com/api/1.1/todo-lists',settings)
+        let promise =axios.get("social-network.samuraijs.com/api/1.1/todo-lists",settings)
 
         promise.then((res)=>{
                 setState(res.data)
@@ -25,47 +25,47 @@ export const GetTodolists = () => {
     return <div>{JSON.stringify(state)}</div>;
 };
 
-export const CreateTodolists = () => {
-    const { state, setState } = useState<any>(null);
-    useEffect(() => {
-        let promise =axios.get('http://social-network.samuraijs.com/api/1.1/todo-lists',{title: "StoryBookTodoList"},settings)
-
-        promise.then((res)=>{
-            setState(res.data)
-        })
-    },[]);
-    return <div>{JSON.stringify(state)}</div>;
-};
-export const UpdateTodolists = () => {
-    const { state, setState } = useState<any>(null);
-    useEffect(() => {
-        const todolistId = "v1()"
-        let promise =axios.delet('http://social-network.samuraijs.com/api/1.1/todo-lists',{title: "StoryBookTodoList"},settings)
-
-        promise.then((res)=>{
-            setState(res.data)
-        })
-    },[]);
-
-    return <div>{JSON.stringify(state)}</div>;
-}
-export const DeleteTodolists = () => {
-    const { state, setState } = useState<any>(null);
-
-    useEffect(() => {
-        const todolistId = "v1()"; // Замените на актуальный ID списка задач
-
-        const deleteTodoList = async () => {
-            try {
-                await axios.delete(`http://social-network.samuraijs.com/api/1.1/todo-lists/${todolistId}`);
-                setState({ message: 'Список задач успешно удален' });
-            } catch (error) {
-                console.error('Ошибка при удалении списка задач:', error);
-            }
-        };
-
-        deleteTodoList();
-    }, []);
-
-    return <div>{JSON.stringify(state)}</div>;
-};
+// export const CreateTodolists = () => {
+//     const { state, setState } = useState<any>(null);
+//     useEffect(() => {
+//         let promise =axios.get('http://social-network.samuraijs.com/api/1.1/todo-lists',{title: "StoryBookTodoList"},settings)
+//
+//         promise.then((res)=>{
+//             setState(res.data)
+//         })
+//     },[]);
+//     return <div>{JSON.stringify(state)}</div>;
+// };
+// export const UpdateTodolists = () => {
+//     const { state, setState } = useState<any>(null);
+//     useEffect(() => {
+//         const todolistId = "v1()"
+//         let promise =axios.delet('http://social-network.samuraijs.com/api/1.1/todo-lists',{title: "StoryBookTodoList"},settings)
+//
+//         promise.then((res)=>{
+//             setState(res.data)
+//         })
+//     },[]);
+//
+//     return <div>{JSON.stringify(state)}</div>;
+// }
+// export const DeleteTodolists = () => {
+//     const { state, setState } = useState<any>(null);
+//
+//     useEffect(() => {
+//         const todolistId = "v1()"; // Замените на актуальный ID списка задач
+//
+//         const deleteTodoList = async () => {
+//             try {
+//                 await axios.delete(`http://social-network.samuraijs.com/api/1.1/todo-lists/${todolistId}`);
+//                 setState({ message: 'Список задач успешно удален' });
+//             } catch (error) {
+//                 console.error('Ошибка при удалении списка задач:', error);
+//             }
+//         };
+//
+//         deleteTodoList();
+//     }, []);
+//
+//     return <div>{JSON.stringify(state)}</div>;
+// };

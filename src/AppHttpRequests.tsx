4257a6d7a5c9db817766cc7c6e23fd954fb6eb34
SirMissir <pkv_96@icloud.com>
2024-11-08 21:28:43 +0300

    refactor: add DELETE tasks request, type

--- src/AppHttpRequests.tsx
+++ src/AppHttpRequests.tsx
@@ -1,12 +1,10 @@
 import Checkbox from '@mui/material/Checkbox'
-import React, { ChangeEvent, useEffect, useState } from 'react'
+import React, {ChangeEvent, useEffect, useState} from 'react'
 // import { AddItemForm } from '../common/components/AddItemForm/AddItemForm'
 // import { EditableSpan } from '../common/components/EditableSpan/EditableSpan'
 import AddItemForms from "./AddItemForms";
 import EditableSpan from "./EditableSpan";
 import axios from "axios";
-
-
 
 
 export type Todolist = {
@@ -19,15 +17,15 @@ type FieldError = {
     error: string
     field: string
 }
-type CreateTodolistResponse ={
+type CreateTodolistResponse = {
     data: {
         item: Todolist
     },
-    fieldsErrors:[FieldError],
-    messages:string,
-    resultCode:number
+    fieldsErrors: [FieldError],
+    messages: string,
+    resultCode: number
 }
-type DeleteTodolistResponse ={
+type DeleteTodolistResponse = {
     resultCode: number
     messages: Array<string>,
     data: {}
@@ -59,7 +57,7 @@ export type GetTasksResponse = {
 export type CreateTaskResponse = {
     resultCode: number
     messages: string
-    data:{
+    data: {
         item: {
             description: string
             title: string
@@ -73,11 +71,19 @@ export type CreateTaskResponse = {
             order: number
             addedDate: string
 
-        }}
-
+        }
     }
 
-export type UpdateTaskResponse ={
+}
+export type DeleteTaskResponse = {
+    resultCode: number
+    messages: string
+    data: {}
+    fieldsErrors: [FieldError]
+
+}
+
+export type UpdateTaskResponse = {
     resultCode: number
     messages: string,
     data: {}
@@ -87,9 +93,9 @@ export type UpdateTaskModel = {
     description: string
     completed: boolean
     status: number
-    priority:number
-    startDate:string
-    deadline:string
+    priority: number
+    startDate: string
+    deadline: string
 }
 
 export const AppHttpRequests = () => {
@@ -115,7 +121,7 @@ export const AppHttpRequests = () => {
                             },
                         })
                         .then(res => {
-                            setTasks({ ...tasks, [tl.id]: res.data.items })
+                            setTasks({...tasks, [tl.id]: res.data.items})
                         })
                 })
             })
@@ -123,9 +129,9 @@ export const AppHttpRequests = () => {
 
     const createTodolistHandler = (title: string) => {
         axios
-            .post<CreateTodolistResponse>   (
+            .post<CreateTodolistResponse>(
                 'https://social-network.samuraijs.com/api/1.1/todo-lists',
-                { title },
+                {title},
                 {
                     headers: {
 
@@ -147,7 +153,7 @@ export const AppHttpRequests = () => {
             })
             .then(res => {
                 debugger
-                setTodolists(data=>data.filter(t=>t.id !==id))
+                setTodolists(data => data.filter(t => t.id !== id))
             })
     }
 
@@ -155,7 +161,7 @@ export const AppHttpRequests = () => {
         axios
             .put<UpdateTodolistResponse>(
                 `https://social-network.samuraijs.com/api/1.1/todo-lists/${id}`,
-                { title },
+                {title},
                 {
                     headers: {
 
@@ -164,7 +170,7 @@ export const AppHttpRequests = () => {
             )
             .then(res => {
                 debugger
-                setTodolists(data=>data.map(t=>t.id===id?{...t,title:title}:t))
+                setTodolists(data => data.map(t => t.id === id ? {...t, title: title} : t))
             })
     }
 
@@ -181,12 +187,27 @@ export const AppHttpRequests = () => {
             )
             .then(res => {
                 const newTask = res.data.data.item
-                setTasks({ ...tasks, [todolistId]: [newTask, ...tasks[todolistId]] })
+                setTasks({...tasks, [todolistId]: [newTask, ...tasks[todolistId]]})
             })
     }
 
     const removeTaskHandler = (taskId: string, todolistId: string) => {
-        // remove task
+        axios
+            .delete<DeleteTaskResponse>(
+                `https://social-network.samuraijs.com/api/1.1//todo-lists/${todolistId}/tasks/${taskId}`,
+                {
+                    headers: {
+                        Authorization: 'Bearer ,
+                        'API-KEY': ',
+                    }
+                },
+            )
+            .then(res => {
+                console.log(res.data)
+                setTasks((data: Record<string, DomainTask[]> )=>({
+                    ...data,[todolistId]:data[todolistId].filter(t=>t.id!==taskId)
+                }))
+            })
     }
 
     const changeTaskStatusHandler = (e: ChangeEvent<HTMLInputElement>, task: DomainTask) => {
@@ -214,54 +235,56 @@ export const AppHttpRequests = () => {
                 }
             )
             .then(res => {
-                const newTasks = tasks[task.todoListId].map((t: DomainTask)=> (t.id === task.id ? { ...t, ...model } : t))
-                setTasks({ ...tasks, [task.todoListId]: newTasks })
+                const newTasks = tasks[task.todoListId].map((t: DomainTask) => (t.id === task.id ? {...t, ...model} : t))
+                setTasks({...tasks, [task.todoListId]: newTasks})
             })
 
     }
 
-    const changeTaskTitleHandler = (title: string, task: any) => {}
+    const changeTaskTitleHandler = (title: string, task: any) => {
+    }
 
-        return (
-            <div style={{ margin: '20px' }}>
-                <AddItemForms addItem={createTodolistHandler} />
+    return (
+        <div style={{margin: '20px'}}>
+            <AddItemForms addItem={createTodolistHandler}/>
 
-                {/* Todolists */}
-                {todolists.map((tl: any) => {
-                    return (
-                        <div key={tl.id} style={todolist}>
-                            <div>
-                                <EditableSpan
-                                    title={tl.title}
-                                    changeTitle={(title: string) => updateTodolistHandler(tl.id, title)}
-                                />
-                                <button onClick={() => removeTodolistHandler(tl.id)}>x</button>
-                            </div>
-                            <AddItemForms addItem={title => createTaskHandler(title, tl.id)} />
-
-                            {/* Tasks */}
-                            {!!tasks[tl.id] &&
-                                tasks[tl.id].map((task: DomainTask) => {
-                                    return (
-                                        <div key={task.id}>
-                                            <Checkbox
-                                                checked={task.status === 2 ? true : false}
-                                                onChange={e => changeTaskStatusHandler(e, task)}
-                                            />
-                                            <EditableSpan
-                                                title={task.title}
-                                                changeTitle={title => changeTaskTitleHandler(title, task)}
-                                            />
-                                            <button onClick={() => removeTaskHandler(task.id, tl.id)}>x</button>
-                                        </div>
-                                    )
-                                })}
+            {/* Todolists */}
+            {todolists.map((tl: any) => {
+                return (
+                    <div key={tl.id} style={todolist}>
+                        <div>
+                            <EditableSpan
+                                title={tl.title}
+                                changeTitle={(title: string) => updateTodolistHandler(tl.id, title)}
+                            />
+                            <button onClick={() => removeTodolistHandler(tl.id)}>x</button>
                         </div>
-                    )
-                })}
-            </div>
-        )
+                        <AddItemForms addItem={title => createTaskHandler(title, tl.id)}/>
+
+                        {/* Tasks */}
+                        {!!tasks[tl.id] &&
+                            tasks[tl.id].map((task: DomainTask) => {
+                                return (
+                                    <div key={task.id}>
+                                        <Checkbox
+                                            checked={task.status === 2 ? true : false}
+                                            onChange={e => changeTaskStatusHandler(e, task)}
+                                        />
+                                        <EditableSpan
+                                            title={task.title}
+                                            changeTitle={title => changeTaskTitleHandler(title, task)}
+                                        />
+                                        <button onClick={() => removeTaskHandler(task.id, tl.id)}>x</button>
+                                    </div>
+                                )
+                            })}
+                    </div>
+                )
+            })}
+        </div>
+    )
 }
+
 
 // Styles
 const todolist: React.CSSProperties = {

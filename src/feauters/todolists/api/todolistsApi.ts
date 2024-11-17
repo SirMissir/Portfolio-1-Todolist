aa8aea69089388808b019119ad00ea0a07e09c0e
SirMissir <pkv_96@icloud.com>
2024-11-17 19:02:01 +0300

    refactor: todolistsApi use instnce

--- src/feauters/todolists/api/todolistsApi.ts
+++ src/feauters/todolists/api/todolistsApi.ts
@@ -1,31 +1,31 @@
 import axios from "axios";
 import {CreateTodolistResponse, DeleteTodolistResponse, Todolist, UpdateTodolistResponse} from "./todolistsApi.types";
 
+const settings = {
+    withCredentials: true,
+    headers: {
+        "API-KEY": ""
+    }
+}
+
+const instance = axios.create({
+    baseURL: 'https://social-network.samuraijs.com/api/1.1/',
+    ...settings
+})
+
 
 export const todolistsApi = {
     getTodolists() {
-        const promise = axios.get<Todolist[]>(
-            'https://social-network.samuraijs.com/api/1.1/todo-lists',
-            {
-                headers: {
-                    Authorization: 'Bearer ',
-                },
-            }
-        )
+        const promise = instance
+            .get<Todolist[]>('todo-lists')
         return promise
     },
 
     createTodolists(title:string) {
-        const promise = axios.post<CreateTodolistResponse>(
-            'https://social-network.samuraijs.com/api/1.1/todo-lists',
-            {title},
-            {
-                headers: {
-                    Authorization:,
-                    'API-KEY':,
-                },
-            }
-        )
+        const promise =  instance
+            .post<CreateTodolistResponse>(
+                'todo-lists',
+                {title: title})
         return promise
     },
 

b5123c2ff5c6f6f08082fcec060b0fad326466b3
SirMissir <pkv_96@icloud.com>
2024-12-18 23:50:37 +0300

    refactor:  create Tasks

--- src/feauters/todolists/ui/Todolists/Todolist/Tasks/Task/Task .tsx
+++ src/feauters/todolists/ui/Todolists/Todolist/Tasks/Task/Task .tsx
@@ -1,9 +1,9 @@
 import React, { ChangeEvent, memo } from 'react';
 import Checkbox from "@mui/material/Checkbox";
-import EditableSpan from "../../../../../../common/components/EditableSpan/EditableSpan";
+import EditableSpan from "../../../../../../../common/components/EditableSpan/EditableSpan";
 import IconButton from "@mui/material/IconButton";
 import DeleteIcon from "@mui/icons-material/Delete";
-import { TaskType } from "../../TodoListOld";
+import { TaskType } from "../../../TodoListOld";
 
 type TaskPropsType = {
     task: TaskType;

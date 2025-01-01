a45d6264f6f24cb8901deb692da51d5a2856f71e
SirMissir <pkv_96@icloud.com>
2025-01-01 23:54:15 +0300

    refactor: add import app reducer

--- src/app/store.ts
+++ src/app/store.ts
@@ -1,12 +1,14 @@
 import { tasksReducer } from '../feauters/todolists/model/tasks-reducer'
 import { todolistsReducer } from '../feauters/todolists/model/todolists-reducer'
 import {combineReducers,legacy_createStore as createStore} from 'redux'
+import {appReducer} from "./app-reducer";
 
 // объединяя reducer-ы с помощью combineReducers,
 // мы задаём структуру нашего единственного объекта-состояния
 export const rootReducer = combineReducers({
     tasks: tasksReducer,
-    todolists: todolistsReducer
+    todolists: todolistsReducer,
+    app: appReducer,
 })
 // непосредственно создаём store
 export const store = createStore(rootReducer)

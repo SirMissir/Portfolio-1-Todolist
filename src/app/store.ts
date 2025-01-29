3edfd30350c5478430b1187a2ce10b291f5f46f4
SirMissir <pkv_96@icloud.com>
2025-01-29 23:25:09 +0300

    refactor: Removing duplicate code and improving Redux store typing

--- src/app/store.ts
+++ src/app/store.ts
@@ -3,17 +3,18 @@ import { todolistsReducer } from '../feauters/todolists/model/todolists-reducer'
 import {combineReducers,legacy_createStore as createStore} from 'redux'
 import {appReducer} from "./app-reducer";
 
-// объединяя reducer-ы с помощью combineReducers,
-// мы задаём структуру нашего единственного объекта-состояния
+
 export const rootReducer = combineReducers({
     tasks: tasksReducer,
     todolists: todolistsReducer,
     app: appReducer,
 })
-// непосредственно создаём store
+
 export const store = createStore(rootReducer)
-// определить автоматически тип всего объекта состояния
-export type AppRootStateType = ReturnType<typeof rootReducer>
-// а это, чтобы можно было в консоли браузера обращаться к store в любой момент
+
+export type RootState = ReturnType<typeof store.getState>
+
+export type AppDispatch = typeof store.dispatch
+//  это, чтобы можно было в консоли браузера обращаться к store в любой момент
 // @ts-ignore
 window.store = store
